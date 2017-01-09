0863b09ebcd89fb0a24af41b2d7e703f96817a35
Aaronrami <aaron.ramirezp@hotmail.com>
2017-01-09 15:03:01 -0500

    Movimiento Personaje
    Movimiento personaje

---
+++ Assets/Scripts/ControlPersonaje.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPersonaje : MonoBehaviour {
+	public float velocidad = 1f;
+	Rigidbody2D rgb;
+	// Use this for initialization
+	void Start () {
+		rgb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		Vector2 v = new Vector2 (velocidad, 0);
+		rgb.velocity = v;
+	}
+}
